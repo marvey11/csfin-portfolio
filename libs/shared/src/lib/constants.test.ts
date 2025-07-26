217dc00e8dd50e5533a7eb09f2a94d4d72f86a14
Marco Wegner <673439+marvey11@users.noreply.github.com>
2025-07-26 05:11:31 +0200

    feat: optimise main app flow

--- libs/shared/src/lib/constants.test.ts
+++ libs/shared/src/lib/constants.test.ts
@@ -1,5 +1,7 @@
+import { FLOATING_POINT_TOLERANCE } from "./constants.js";
+
 describe("Dummy Test Suite", () => {
   it("should pass a dummy test", () => {
-    expect(true).toStrictEqual(true);
+    expect(FLOATING_POINT_TOLERANCE).toBeCloseTo(0.000001);
   });
 });
