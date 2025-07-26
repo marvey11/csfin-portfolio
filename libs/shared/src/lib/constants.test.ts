645cc92182d8e9cb7318b5521e62d1dd6a99ecdd
Marco Wegner <673439+marvey11@users.noreply.github.com>
2025-07-26 21:54:45 +0200

    chore: remove shared lib

--- libs/shared/src/lib/constants.test.ts
+++
@@ -1,7 +0,0 @@
-import { FLOATING_POINT_TOLERANCE } from "./constants.js";
-
-describe("Dummy Test Suite", () => {
-  it("should pass a dummy test", () => {
-    expect(FLOATING_POINT_TOLERANCE).toBeCloseTo(0.000001);
-  });
-});
