271079ba544b6f39948c8c65e1a930b277976fe5
Marco Wegner <673439+marvey11@users.noreply.github.com>
2025-07-27 23:47:06 +0200

    Revert "chore: set up projects for web app"

--- libs/shared/data-models/src/lib/data-models.spec.ts
+++
@@ -1,7 +0,0 @@
-import { dataModels } from "./data-models.js";
-
-describe("dataModels", () => {
-  it("should work", () => {
-    expect(dataModels()).toEqual("data-models");
-  });
-});
--- libs/shared/ui/src/lib/ui.tsx
+++
@@ -1,9 +0,0 @@
-export function CsfinPortfolioUi() {
-  return (
-    <div>
-      <h1>Welcome to CsfinPortfolioUi!</h1>
-    </div>
-  );
-}
-
-export default CsfinPortfolioUi;
