a8aff8bae929ed5b4f98c08e8b88f2bfbb7134f9
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-08-31 10:30:58 +0000

    chore: update workflows

---
+++ apps/portfolio/jest.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  displayName: "@csfin-portfolio/portfolio",
+  preset: "../../jest.preset.js",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/libs/core",
+};
